51afffd917193bd325ac3db6446d0ddcb206b293
gabrielztk <gabrielztk@al.insper.edu.br>
2021-02-27 22:47:52 -0300

    Roteiros 1 e 2

---
+++ Assets/Scripts/MovimentoRaquete.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoRaquete : MonoBehaviour
+{
+    [Range(1, 30)]
+    public float velocidade;
+    GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.GetInstance();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // primeira execução nas rotinas de Update() da Bola e Raquete.
+        if (gm.gameState != GameManager.GameState.GAME) return;
+        float inputX = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(inputX, 0, 0) * Time.deltaTime * velocidade;
+
+        if(Input.GetKeyDown(KeyCode.Escape) && gm.gameState == GameManager.GameState.GAME) {
+            gm.ChangeState(GameManager.GameState.PAUSE);
+        }
+    }
+}
